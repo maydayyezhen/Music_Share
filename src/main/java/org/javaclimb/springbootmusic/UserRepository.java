4e1873717a7e8c77dfa03eeb800af05ccf7737a8
kagari7 <133572575+kagari7@users.noreply.github.com>
2025-04-10 11:37:59 +0800

    Delete src/main/java/org/javaclimb/springbootmusic/UserRepository.java

--- src/main/java/org/javaclimb/springbootmusic/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.javaclimb.springbootmusic.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.javaclimb.springbootmusic.model.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-    User findByUsername(String username);
-    boolean existsByUsername(String username);
-}
