f762e1979371f5e66c543a108ea754ba267137e5
kagari7 <133572575+kagari7@users.noreply.github.com>
2025-04-11 14:01:17 +0800

    Update UserService.java

--- src/main/java/org/javaclimb/springbootmusic/service/UserService.java
+++ src/main/java/org/javaclimb/springbootmusic/service/UserService.java
@@ -1,6 +1,5 @@
 package org.javaclimb.springbootmusic.service;
 
-import jakarta.persistence.EntityNotFoundException;
 import org.javaclimb.springbootmusic.model.User;
 import org.javaclimb.springbootmusic.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
@@ -20,42 +19,44 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
-    public User getUserById(Integer id) {
-        return userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("用户未找到: " + id));
+
+    public User getUserByUserName(String name) {
+        return userRepository.findByUsername(name);
     }
 
     public User createUser(User user) {
         return userRepository.save(user);
     }
-    public void deleteUserById(Integer id) {
-        userRepository.deleteById(id);
+    public void deleteUserByUserName(String name) {
+        userRepository.delete(getUserByUserName(name));
     }
 
-    public User updateUser(Integer id, String nickname) {
-        User user = getUserById(id);
+    public User updateUser(String name, String nickname) {
+        User user = getUserByUserName(name);
 
         user.setNickname(nickname);
         return userRepository.save(user);
     }
 
-    public User updatePassword(Integer userId, String oldPassword, String newPassword) {
-        User user = getUserById(userId);
+    public User updatePassword(String name, String oldPassword, String newPassword) {
+        User user = getUserByUserName(name);
         user.setPassword(newPassword);
         return userRepository.save(user);
     }
 
-    public ResponseEntity<Void> uploadAvatarFile(Integer id, MultipartFile avatarFile) {
-        User user = getUserById(id);
+    public ResponseEntity<Void> uploadAvatarFile(String name, MultipartFile avatarFile) {
+        User user = getUserByUserName(name);
         user.setAvatarUrl(uploadFile(avatarFile, USER_AVATAR_PATH));
         userRepository.save(user);
         return ResponseEntity.ok().build();
     }
 
-    public ResponseEntity<String> deleteAvatarFileById(Integer id) {
-        User user = getUserById(id);
+    public ResponseEntity<String> deleteAvatarFileById(String name) {
+        User user = getUserByUserName(name);
         String fileUrl = user.getAvatarUrl();
         return FileService.deleteFile(fileUrl);
     }
