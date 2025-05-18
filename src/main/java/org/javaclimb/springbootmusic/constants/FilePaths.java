9555a5bee6ae728c561f3da9bda12e08a74fca36
kagari7 <133572575+kagari7@users.noreply.github.com>
2025-05-18 18:47:03 +0800

    Update FilePaths.java

--- src/main/java/org/javaclimb/springbootmusic/constants/FilePaths.java
+++ src/main/java/org/javaclimb/springbootmusic/constants/FilePaths.java
@@ -8,11 +8,13 @@ public class FilePaths {
     public static final String ALBUMS_PATH = UPLOAD_PATH + "albums/";  // 专辑上传路径
     public static final String ARTISTS_PATH = UPLOAD_PATH + "artists/";  // 歌手上传路径
     public static final String USERS_PATH = UPLOAD_PATH + "users/";  // 用户上传路径
-
+    public static final String PLAYLIST_PATH = UPLOAD_PATH + "playlists/";  // 歌单上传路径
+    
     public static final String SONG_AUDIO_PATH = SONGS_PATH + "audio/";  // 歌曲音频路径
     public static final String SONG_LYRIC_PATH = SONGS_PATH + "lyric/";  // 歌曲歌词路径
     public static final String ALBUM_COVER_PATH = ALBUMS_PATH + "cover/";  // 专辑封面路径
     public static final String ARTIST_AVATAR_PATH = ARTISTS_PATH + "avatar/";  // 歌手头像路径
     public static final String USER_AVATAR_PATH = USERS_PATH + "avatar/";  // 用户头像路径
+    public static final String PLAYLIST_COVER_PATH = PLAYLIST_PATH + "cover/";  // 歌单封面路径
 }
 
